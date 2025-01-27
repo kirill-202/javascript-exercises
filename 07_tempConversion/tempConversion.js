aa685045c330af2c740ce40940af5657d9bdb063
Kirill <kirillgoodline@gmail.com>
2025-01-27 04:30:32 +0400

    do exercies 1-7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farTemp) {
+  const toCel = (farTemp -32) *5 /9;
+
+  if (toCel === Math.floor(toCel)) {
+    return parseFloat(toCel.toFixed(0));
+  }
+  return parseFloat(toCel.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celTemp) {
+  const toFar = celTemp * 9 / 5 + 32;
+
+  if (toFar === Math.floor(toFar)) {
+    return parseFloat(toFar.toFixed(0));
+  }
+  return parseFloat(toFar.toFixed(1));
 };
 
 // Do not edit below this line
@@ -9,3 +21,6 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+
+console.log(convertToCelsius(100));
